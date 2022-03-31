b70bf3853325c29286669abc01fed195e870df65
Jon Carifio <carifio24@gmail.com>
2022-03-31 10:43:04 -0400

    Add some basic forms that we'll need.

---
+++ src/components/forms/StudentSignIn.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="student-sign-in">
+    <v-card>
+      <v-card-title>Student Sign In</v-card-title>
+      <v-form
+        ref="form"
+        class="student-signin-form"
+        v-model="valid"
+        lazy-validation
+      >
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="Email"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          :rules="passwordRules"
+          label="Password"
+          type="password"
+          required
+        ></v-text-field>
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="validateAndSubmit"
+        >validate</v-btn>
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator';
+import { emailRules, passwordRules } from '../utils/rules';
+import FormBase from './FormBase.vue';
+import { mapActions } from 'vuex';
+
+@Component
+export default class StudentSignIn extends FormBase {
+
+  valid = true;
+  email = "";
+  password = "";
+  emailRules = emailRules;
+  passwordRules = passwordRules;
+
+  checkStudentSignIn!: (email: string, password: string) => Promise<boolean>;
+
+  beforeCreate(): void {
+    this.$options.methods = {
+      ...this.$options.methods,
+      ...mapActions("cosmicds", ["checkStudentSignIn"])
+    };
+  }
+
+  async submit(): Promise<boolean> {
+    return this.checkStudentSignIn(this.email, this.password);
+  }
+
+  async validateAndSubmit(): Promise<void> {
+    const valid = this.validate();
+    if (valid) {
+      await this.submit();
+      this.reset();
+    }
+  }
+}
+
+</script>
---
+++ src/components/forms/StudentSignUp.vue
@@ -0,0 +1,149 @@
+<template>
+<div id="student-sign-up">
+  <v-card>
+    <v-card-title>Student Sign Up</v-card-title>
+    <v-form
+      ref="form"
+      class="student-signup-form"
+      v-model="valid"
+      lazy-validation
+    >
+      <v-text-field
+        v-model="firstName"
+        :rules="nameRules"
+        label="First name"
+        color="purple darken-2"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="lastName"
+        :rules="nameRules"
+        label="Last name"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="Email"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        :rules="passwordRules"
+        label="Password"
+        type="password"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="institution"
+        :rules="nameRules"
+        label="Educational institution"
+      ></v-text-field>
+      <v-text-field
+        v-model="age"
+        hide-details
+        single-line
+        label="Age"
+        type="number"
+      ></v-text-field>
+      <v-select
+        v-model="gender"
+        :items="['Male', 'Female', 'Other']"
+        label="Gender"
+      ></v-select>
+      <v-alert
+        v-if="errorMessage"
+        color="red lighten-2"
+        >{{ errorMessage }}</v-alert>
+      <v-alert
+        v-if="successMessage"
+        color="green lighten-2"
+        >{{ successMessage }}</v-alert>
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="validateAndSubmit"
+      >validate</v-btn>
+    </v-form>
+  </v-card>
+</div>
+</template>
+
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator';
+import { emailRules, nameRules, passwordRules } from '../utils/rules';
+import FormBase from './FormBase.vue';
+import { mapActions } from 'vuex';
+
+import { StudentData } from '../store/cosmicds';
+
+@Component
+export default class StudentSignUp extends FormBase {
+
+  // firstName = "";
+  // lastName = "";
+  // institution = "";
+  // email = "";
+  // password = "";
+  // age = null;
+  // gender = "";
+
+  firstName = "Student"
+  lastName = "One"
+  institution = "School A"
+  email = "student1@schoola.edu";
+  password = "abc123";
+  age = 18;
+  gender = "Female";
+
+  valid = true;
+  nameRules = nameRules;
+  emailRules = emailRules;
+  passwordRules = passwordRules;
+
+  errorMessage = "";
+  successMessage = "";
+
+  getVerificationURL!: (code: string) => string;
+  submitStudentSignUp!: (data: StudentData) => Promise<any>;
+
+  beforeCreate(): void {
+    this.$options.methods = {
+      ...this.$options.methods,
+      ...mapActions("cosmicds", ["submitStudentSignUp"])
+    };
+  }
+
+  submit() {
+    return this.submitStudentSignUp({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      institution: this.institution,
+      password: this.password,
+      age: this.age,
+      gender: this.gender  
+    });
+  }
+
+  async validateAndSubmit(): Promise<void> {
+    this.errorMessage = "";
+    this.successMessage = "";
+    const valid = this.validate();
+    if (valid) {
+      const result = await this.submit();
+      const code = result.data.status;
+      if (code === 'ok') {
+        this.reset();
+        this.successMessage = "Your account was created successfully. A verification link will be sent to your email."
+      } else if (code === 'email_already_exists') {
+        this.errorMessage = "An account with this email already exists";
+      } else {
+        this.errorMessage = "An error occurred. We apologize for the inconvenience"
+      }
+    }
+  }
+}
+</script>
